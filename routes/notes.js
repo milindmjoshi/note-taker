f62f025882bce25c8a843e7ac75bfd2c2c5414e3
Milman <milindmjoshi@live.com>
2024-03-10 16:09:29 -0400

    Get everything working except for delete

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,31 @@
 const notesRouter = require('express').Router();
 const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend} = require('../helpers/fsUtils.js');
 
 notesRouter.get('/',(req,res)=>{
     console.log('note route');
+    //readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    fs.readFile('./db/db.json',(err,data)=>{
+        if(err) throw err;
+        console.log("Data: " + data);
+        res.json(JSON.parse(data))
+    })
 })
+
+notesRouter.post('/', (req,res)=>{
+    console.log(`Got a ${req.method} method`);
+    const {title, text} = req.body;
+    console.log(`Title: ${title}, Text: ${text} `);
+    const note = {
+        id: uuidv4(),
+        title: title,
+        text: text
+    }
+    
+    readAndAppend(note,'./db/db.json');
+    res.status(200).send("Note added");
+ })
 
 module.exports = notesRouter;
